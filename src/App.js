c2e2fe8054e72a61e28b736f6d7764d3c4fbe652
rajvir-dev <makwanarajveer99@gmail.com>
2024-03-19 03:56:27 +0530

    commit6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-export default function App(){
-
-  const [backendData, setBackendData] = useState([{}])
+export default function App() {
+  const [backendData, setBackendData] = useState([{}]);
 
   useEffect(() => {
-    fetch("/users").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  }, [])
-  
+    fetch("/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
 
-  return(
+  return (
     <>
-    
+      {typeof backendData.users === "undefined" ? (
+        <p>loading</p>
+      ) : (
+        backendData.users.map((user, i) => <p key={i}>{user}</p>)
+      )}
     </>
-  )
+  );
 }
